2105101c439e42fa56a6ec541632987bcd858a18
Sergei <Sergei@Comp.Dlink>
2013-12-30 22:35:51 +0200

    Use different handlers for different domain objects

--- src/main/java/org/training/issuetracker/domain/DAO/DefaultDAO.java
+++
@@ -1,8 +0,0 @@
-package org.training.issuetracker.domain.DAO;
-
-import java.util.Map;
-import org.training.issuetracker.exceptions.DaoException;
-
-public interface DefaultDAO {
-	public <T> Map<Long, T> getResolutionMap() throws DaoException;
-}
