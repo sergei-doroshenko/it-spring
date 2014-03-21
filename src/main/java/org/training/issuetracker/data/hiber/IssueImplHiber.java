df29289a62338c182897d1f4c145a7f034e24213
Sergei Doroshenko <6823298@gmail.com>
2014-03-21 12:49:23 +0300

    Fix search.

--- src/main/java/org/training/issuetracker/data/hiber/IssueImplHiber.java
+++ src/main/java/org/training/issuetracker/data/hiber/IssueImplHiber.java
@@ -75,6 +75,40 @@ public class IssueImplHiber implements IssueDAO {
 		return Collections.checkedList(result, Issue.class);
 	}
 	
+	public DetachedCriteria addSearch (DetachedCriteria criteria, String filters) throws NumberFormatException, DaoException {
+		if(filters != null && !filters.isEmpty()) {
+			logger.debug(filters);
+			SearchFilter filter = new JSONDeserializer<SearchFilter>().deserialize(filters, SearchFilter.class);
+			logger.debug("Filter " + filter.toString());
+			List<SearchRule> rules = filter.getRules();
+			Map<String, Set<SearchRule>> checkedRules = checkDoubles(rules);
+			
+			//Key is field
+			for (String key : checkedRules.keySet()) {
+				Set<SearchRule> rulesSet = checkedRules.get(key);
+				boolean isMultiRules = rulesSet.size() > 1;
+
+				//Add OR
+				//"createBy" "modifyBy" "assignee" "status" "resolution" "priority" "type" ['eq', 'ne']
+				//"summary" "project" "build" ['bw', 'ew', 'cn']
+				//"id" "createDate" "modifyDate"
+				
+				if (isMultiRules) {
+					addDisjSearch(criteria, rulesSet);
+				} else {
+					addConjSearch(criteria, rulesSet);
+				}
+			}
+		}
+		
+		return criteria;
+		
+	}
+	
+	/**Checks if search rules are duplicated.
+	 * @param rules
+	 * @return
+	 */
 	public Map<String, Set<SearchRule>> checkDoubles (List<SearchRule> rules) {
 		Map<String, Set<SearchRule>> checked = new HashMap<String, Set<SearchRule>>();
 		for (SearchRule rule : rules) {
@@ -90,165 +124,290 @@ public class IssueImplHiber implements IssueDAO {
 		return checked;
 	}
 	
-	public DetachedCriteria addSearch (DetachedCriteria criteria, String filters) throws NumberFormatException, DaoException {
-		if(filters != null && !filters.isEmpty()) {
-			logger.debug(filters);
-			SearchFilter filter = new JSONDeserializer<SearchFilter>().deserialize(filters, SearchFilter.class);
-			logger.debug("Filter " + filter.toString());
-			List<SearchRule> rules = filter.getRules();
-			Map<String, Set<SearchRule>> checkedRules = checkDoubles(rules);
+	/**Method add search if search parameters duplicated.
+	 * @param criteria
+	 * @param rulesSet
+	 * @throws NumberFormatException
+	 * @throws DaoException
+	 */
+	public void addDisjSearch (DetachedCriteria criteria, Set<SearchRule> rulesSet) throws NumberFormatException, DaoException {
+		Disjunction disjunction = Restrictions.disjunction();
+		for (SearchRule rule : rulesSet) {
+			String ruleOp = rule.getOp();
+			String field = rule.getField();
+			String data = rule.getData();
+			Object value = getSearchValue(field, data);
 			
-			//Key is field
-			for (String key : checkedRules.keySet()) {
-				Set<SearchRule> rulesSet = checkedRules.get(key);
+			if (field.equals("createBy") || field.equals("modifyBy") || field.equals("assignee")
+					|| field.equals("status") || field.equals("resolution") || field.equals("priority")
+					|| field.equals("type")) {
 					
-				if(key.equals("project")) {
+				addSearchDisjEqNe(disjunction, ruleOp, field, value);
+					
+			} else if (field.equals("summary") || field.equals("project") || field.equals("build")) {
+				
+				if(field.equals("project")) {
 					criteria.createAlias("issue.project", "project");
 				}
-				if(key.equals("build")) {
+				if(field.equals("build")) {
 					criteria.createAlias("issue.build", "build");
 				}
-				//Add OR
-				if (rulesSet.size() > 1) {
-					Disjunction disjunction = Restrictions.disjunction();
-					for (SearchRule rule : rulesSet) {
-						
-						String field = rule.getField();
-						String data = rule.getData();
-						Object value = getSearchValue(field, data);
-						switch (rule.getOp()) {
-							case "eq" : {
-								disjunction.add(Restrictions.eq(field, value));
-								break;
-							}
-							case "ne" : {
-								disjunction.add(Restrictions.ne(field, value));
-								break;
-							}
-							case "lt" : {
-								disjunction.add(Restrictions.lt(field, value));
-								break;
-							}
-							case "le" : {
-								disjunction.add(Restrictions.le(field, value));
-								break;
-							}
-							case "gt" : {
-								disjunction.add(Restrictions.gt(field, value));
-								break;
-							}
-							case "ge" : {
-								disjunction.add(Restrictions.ge(field, value));
-								break;
-							}
-							case "bw" : {
-								if(field.equals("project")) {
-									disjunction.add(Restrictions.like("project.name", (String) value, MatchMode.START));
-								} else if (field.equals("build")) {
-									disjunction.add(Restrictions.like("build.name", (String) value, MatchMode.START));
-								} else {
-									disjunction.add(Restrictions.like(field, (String) value, MatchMode.START));
-								}
-								break;
-							}
-							case "ew" : {
-								if(field.equals("project")) {
-									disjunction.add(Restrictions.like("project.name", (String) value, MatchMode.END));
-								} else if (field.equals("build")) {
-									disjunction.add(Restrictions.like("build.name", (String) value, MatchMode.END));
-								} else {
-									disjunction.add(Restrictions.like(field, (String) value, MatchMode.END));
-								}
-								break;
-							}
-							case "cn" : {
-								if(field.equals("project")) {
-									disjunction.add(Restrictions.like("project.name", (String) value, MatchMode.ANYWHERE));
-								} else if (field.equals("build")) {
-									disjunction.add(Restrictions.like("build.name", (String) value, MatchMode.ANYWHERE));
-								} else {
-									disjunction.add(Restrictions.like(field, (String) value, MatchMode.ANYWHERE));
-								}
-								break;
-							}
-							default : {
-								break;
-							}
-						};
-					}
-					criteria.add(disjunction);
 					
-				} else {
-					for (SearchRule rule : rulesSet) {
-						
-						String field = rule.getField();
-						String data = rule.getData();
-						Object value = getSearchValue(field, data);
-						switch (rule.getOp()) {
-							case "eq" : {
-								criteria.add(Restrictions.eq(rule.getField(), value));
-								break;
-							}
-							case "ne" : {
-								criteria.add(Restrictions.ne(field, value));
-								break;
-							}
-							case "lt" : {
-								criteria.add(Restrictions.lt(field, value));
-								break;
-							}
-							case "le" : {
-								criteria.add(Restrictions.le(field, value));
-								break;
-							}
-							case "gt" : {
-								criteria.add(Restrictions.gt(field, value));
-								break;
-							}
-							case "ge" : {
-								criteria.add(Restrictions.ge(field, value));
-								break;
-							}
-							case "bw" : {
-								if(field.equals("project")) {
-									criteria.add(Restrictions.like("project.name", (String) value, MatchMode.START));
-								} else if (field.equals("build")) {
-									criteria.add(Restrictions.like("build.name", (String) value, MatchMode.START));
-								} else {
-									criteria.add(Restrictions.like(field, (String) value, MatchMode.START));
-								}
-								break;
-							}
-							case "ew" : {
-								if(field.equals("project")) {
-									criteria.add(Restrictions.like("project.name", (String) value, MatchMode.END));
-								} else if (field.equals("build")) {
-									criteria.add(Restrictions.like("build.name", (String) value, MatchMode.END));
-								} else {
-									criteria.add(Restrictions.like(field, (String) value, MatchMode.END));
-								}
-								break;
-							}
-							case "cn" : {
-								if(field.equals("project")) {
-									criteria.add(Restrictions.like("project.name", (String) value, MatchMode.ANYWHERE));
-								} else if (field.equals("build")) {
-									criteria.add(Restrictions.like("build.name", (String) value, MatchMode.ANYWHERE));
-								} else {
-									criteria.add(Restrictions.like(field, (String) value, MatchMode.ANYWHERE));
-								}
-								break;
-							}
-							default : {
-								break;
-							}
-						};
-					}
-				}
+				addSearchDisjBwEwCn(disjunction, ruleOp, field, value);
+					
+			} else if (field.equals("id") || field.equals("createDate") || field.equals("modifyDate")) {
+					
+				addSearchDisjEqNeLtLeGtGe(disjunction, ruleOp, field, value);
+					
 			}
 		}
-		
-		return criteria;
+		criteria.add(disjunction);
+	}
+	
+	/**Method add search if search parameters isn't duplicated.
+	 * @param criteria
+	 * @param rulesSet
+	 * @throws NumberFormatException
+	 * @throws DaoException
+	 */
+	public void addConjSearch (DetachedCriteria criteria, Set<SearchRule> rulesSet) throws NumberFormatException, DaoException{
+		for (SearchRule rule : rulesSet) {
+			
+			String ruleOp = rule.getOp();
+			String field = rule.getField();
+			String data = rule.getData();
+			Object value = getSearchValue(field, data);
+			
+			if (field.equals("createBy") || field.equals("modifyBy") || field.equals("assignee")
+					|| field.equals("status") || field.equals("resolution") || field.equals("priority")
+					|| field.equals("type")) {
+					
+				addSearchEqNe(criteria, ruleOp, field, value);
+					
+			} else if (field.equals("summary") || field.equals("project") || field.equals("build")) {
+				
+				if(field.equals("project")) {
+					criteria.createAlias("issue.project", "project");
+				}
+				if(field.equals("build")) {
+					criteria.createAlias("issue.build", "build");
+				}
+					
+				addSearchBwEwCn(criteria, ruleOp, field, value);
+					
+			} else if (field.equals("id") || field.equals("createDate") || field.equals("modifyDate")) {
+					
+				addSearchEqNeLtLeGtGe(criteria, ruleOp, field, value);
+					
+			}
+		}
+	}
+	
+	/**Add search by fields "createBy" "modifyBy" "assignee" "status" "resolution" "priority" "type".
+	 * with disjunction.
+	 * @param criteria
+	 * @param ruleOp
+	 * @param field
+	 * @param value
+	 */
+	public void addSearchEqNe (DetachedCriteria criteria, String ruleOp, String field, Object value) {
+		switch (ruleOp) {
+			case "eq" : {
+				criteria.add(Restrictions.eq(field, value));
+				break;
+			}
+			case "ne" : {
+				criteria.add(Restrictions.ne(field, value));
+				break;
+			}
+			default : {
+				break;
+			}
+		};
+	}
+	
+	/**Add search by fields "createBy" "modifyBy" "assignee" "status" "resolution" "priority" "type".
+	 * with conjunction.
+	 * @param criteria
+	 * @param ruleOp
+	 * @param field
+	 * @param value
+	 */
+	public void addSearchDisjEqNe (Disjunction disjunction, String ruleOp, String field, Object value) {
+		switch (ruleOp) {
+			case "eq" : {
+				disjunction.add(Restrictions.eq(field, value));
+				break;
+			}
+			case "ne" : {
+				disjunction.add(Restrictions.ne(field, value));
+				break;
+			}
+			default : {
+				break;
+			}
+		};
+	}
+	
+	/**Add search by fields "summary" "project" "build" with conjunction.
+	 * @param criteria
+	 * @param ruleOp
+	 * @param field
+	 * @param value
+	 */
+	public void addSearchBwEwCn (DetachedCriteria criteria, String ruleOp, String field, Object value) {
+		switch (ruleOp) {
+			case "bw" : {
+				if(field.equals("project")) {
+					criteria.add(Restrictions.like("project.name", (String) value, MatchMode.START));
+				} else if (field.equals("build")) {
+					criteria.add(Restrictions.like("build.name", (String) value, MatchMode.START));
+				} else {
+					criteria.add(Restrictions.like(field, (String) value, MatchMode.START));
+				}
+				break;
+			}
+			case "ew" : {
+				if(field.equals("project")) {
+					criteria.add(Restrictions.like("project.name", (String) value, MatchMode.END));
+				} else if (field.equals("build")) {
+					criteria.add(Restrictions.like("build.name", (String) value, MatchMode.END));
+				} else {
+					criteria.add(Restrictions.like(field, (String) value, MatchMode.END));
+				}
+				break;
+			}
+			case "cn" : {
+				if(field.equals("project")) {
+					criteria.add(Restrictions.like("project.name", (String) value, MatchMode.ANYWHERE));
+				} else if (field.equals("build")) {
+					criteria.add(Restrictions.like("build.name", (String) value, MatchMode.ANYWHERE));
+				} else {
+					criteria.add(Restrictions.like(field, (String) value, MatchMode.ANYWHERE));
+				}
+				break;
+			}
+			default : {
+				break;
+			}
+		};
+	}
+	
+	/**Add search by fields "summary" "project" "build" with disjunction.
+	 * @param criteria
+	 * @param ruleOp
+	 * @param field
+	 * @param value
+	 */
+	public void addSearchDisjBwEwCn (Disjunction disjunction, String ruleOp, String field, Object value) {
+		switch (ruleOp) {
+			case "bw" : {
+				if(field.equals("project")) {
+					disjunction.add(Restrictions.like("project.name", (String) value, MatchMode.START));
+				} else if (field.equals("build")) {
+					disjunction.add(Restrictions.like("build.name", (String) value, MatchMode.START));
+				} else {
+					disjunction.add(Restrictions.like(field, (String) value, MatchMode.START));
+				}
+				break;
+			}
+			case "ew" : {
+				if(field.equals("project")) {
+					disjunction.add(Restrictions.like("project.name", (String) value, MatchMode.END));
+				} else if (field.equals("build")) {
+					disjunction.add(Restrictions.like("build.name", (String) value, MatchMode.END));
+				} else {
+					disjunction.add(Restrictions.like(field, (String) value, MatchMode.END));
+				}
+				break;
+			}
+			case "cn" : {
+				if(field.equals("project")) {
+					disjunction.add(Restrictions.like("project.name", (String) value, MatchMode.ANYWHERE));
+				} else if (field.equals("build")) {
+					disjunction.add(Restrictions.like("build.name", (String) value, MatchMode.ANYWHERE));
+				} else {
+					disjunction.add(Restrictions.like(field, (String) value, MatchMode.ANYWHERE));
+				}
+				break;
+			}
+		};
+	}
+	
+	/**Add search by fields "id" "createDate" "modifyDate" with conjunction.
+	 * @param criteria
+	 * @param ruleOp
+	 * @param field
+	 * @param value
+	 */
+	public void addSearchEqNeLtLeGtGe (DetachedCriteria criteria, String ruleOp, String field, Object value) {
+		switch (ruleOp) {
+			case "eq" : {
+				criteria.add(Restrictions.eq(field, value));
+				break;
+			}
+			case "ne" : {
+				criteria.add(Restrictions.ne(field, value));
+				break;
+			}
+			case "lt" : {
+				criteria.add(Restrictions.lt(field, value));
+				break;
+			}
+			case "le" : {
+				criteria.add(Restrictions.le(field, value));
+				break;
+			}
+			case "gt" : {
+				criteria.add(Restrictions.gt(field, value));
+				break;
+			}
+			case "ge" : {
+				criteria.add(Restrictions.ge(field, value));
+				break;
+			}
+			default : {
+				break;
+			}
+		};
+	}
+	
+	/**Add search by fields "id" "createDate" "modifyDate" with disjunction.
+	 * @param criteria
+	 * @param ruleOp
+	 * @param field
+	 * @param value
+	 */
+	public void addSearchDisjEqNeLtLeGtGe (Disjunction disjunction, String ruleOp, String field, Object value) {
+		switch (ruleOp) {
+			case "eq" : {
+				disjunction.add(Restrictions.eq(field, value));
+				break;
+			}
+			case "ne" : {
+				disjunction.add(Restrictions.ne(field, value));
+				break;
+			}
+			case "lt" : {
+				disjunction.add(Restrictions.lt(field, value));
+				break;
+			}
+			case "le" : {
+				disjunction.add(Restrictions.le(field, value));
+				break;
+			}
+			case "gt" : {
+				disjunction.add(Restrictions.gt(field, value));
+				break;
+			}
+			case "ge" : {
+				disjunction.add(Restrictions.ge(field, value));
+				break;
+			}
+			default : {
+				break;
+			}
+		};
 	}
 	
 	public Object getSearchValue (String field, String data) throws NumberFormatException, DaoException {
--- src/main/java/org/training/issuetracker/utils/SearchRule.java
+++ src/main/java/org/training/issuetracker/utils/SearchRule.java
@@ -37,6 +37,43 @@ public class SearchRule {
 	}
 	
 	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((field == null) ? 0 : field.hashCode());
+		result = prime * result + ((op == null) ? 0 : op.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		SearchRule other = (SearchRule) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (field == null) {
+			if (other.field != null)
+				return false;
+		} else if (!field.equals(other.field))
+			return false;
+		if (op == null) {
+			if (other.op != null)
+				return false;
+		} else if (!op.equals(other.op))
+			return false;
+		return true;
+	}
+
+	@Override
 	public String toString() {
 		return "SearchRule [field=" + field + ", op=" + op + ", data=" + data
 				+ "]";
